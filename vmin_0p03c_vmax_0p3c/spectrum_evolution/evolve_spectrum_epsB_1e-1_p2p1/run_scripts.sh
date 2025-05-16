285565974c54b5f2330b542316321b588c936c00
samanthacwu <samantha.c.wu@gmail.com>
2025-05-16 13:29:59 -0700

    trying flatter spectrum (and fixing plot_styles error)

---
+++ vmin_0p03c_vmax_0p3c/spectrum_evolution/evolve_spectrum_epsB_1e-1_p2p1/run_scripts.sh
@@ -0,0 +1,7 @@
+
+# python save_for_spectrum_evolution.py  --evolve_shock_dir '../shell_propagation/' --filename 'shell_evolution_deltat_0.1yr.txt' --data_dir './evolve_spectrum_epsB_1e-1_p2p1/'
+
+# python spectrum_evolution.py './evolve_spectrum_epsB_1e-1_p2p1/shell_evolution_deltat_0.1yr/' './evolve_spectrum_epsB_1e-1_p2p1/shell_evolution_deltat_0.1yr/' --p_exp=2.1 --eps_B=0.1 --eps_E=0.1 --f_omega=1.00 --max_step=100000 --print_int=10000 --dt_sc=1e-3 --t0=0.012 --tf=11
+
+python analyze_multiwavelength_spectrum.py --p_exp=2.1 --f_omega=1.00 --eps_E=0.1 --eps_B=0.1 --Te_csm=1e4 --model_dir './evolve_shock/shell_evolution_deltat_0.1yr/' --evolve_shock_dir './evolve_shock/shell_evolution_deltat_0.1yr/' --data_dir './evolve_spectrum_epsB_1e-1_p2p1/shell_evolution_deltat_0.1yr/' --dNdgamma_dir './evolve_spectrum_epsB_1e-1_p2p1/shell_evolution_deltat_0.1yr/'
+mv Radio_curve*.png ./evolve_spectrum_epsB_1e-1_p2p1/shell_evolution_deltat_0.1yr/
--- vmin_0p03c_vmax_0p3c/spectrum_evolution/run_scripts.sh
+++ vmin_0p03c_vmax_0p3c/spectrum_evolution/run_scripts.sh
@@ -1,7 +1,7 @@
 
-# python save_for_spectrum_evolution.py  --evolve_shock_dir '../shell_propagation/' --filename 'shell_evolution_deltat_0.1yr.txt' --data_dir './evolve_spectrum_epsB_1e-1/'
+# python save_for_spectrum_evolution.py  --evolve_shock_dir '../shell_propagation/' --filename 'shell_evolution_deltat_0.3yr.txt' --data_dir './evolve_spectrum_epsB_1e-1_p2p1/'
 
-# python spectrum_evolution.py './evolve_spectrum_epsB_1e-1/shell_evolution_deltat_0.1yr/' './evolve_spectrum_epsB_1e-1/shell_evolution_deltat_0.1yr/' --p_exp=3 --eps_B=0.1 --eps_E=0.1 --f_omega=1.00 --max_step=100000 --print_int=10000 --dt_sc=1e-3 --t0=0.012 --tf=11
+# python spectrum_evolution.py './evolve_spectrum_epsB_1e-1_p2p1/shell_evolution_deltat_0.3yr/' './evolve_spectrum_epsB_1e-1_p2p1/shell_evolution_deltat_0.3yr/' --p_exp=2.1 --eps_B=0.1 --eps_E=0.1 --f_omega=1.00 --max_step=100000 --print_int=10000 --dt_sc=1e-3 --t0=0.034 --tf=33.
 
-python analyze_multiwavelength_spectrum.py --p_exp=3 --f_omega=1.00 --eps_E=0.1 --eps_B=0.1 --Te_csm=1e4 --model_dir './evolve_shock/shell_evolution_deltat_0.1yr/' --evolve_shock_dir './evolve_shock/shell_evolution_deltat_0.1yr/' --data_dir './evolve_spectrum_epsB_1e-1/shell_evolution_deltat_0.1yr/' --dNdgamma_dir './evolve_spectrum_epsB_1e-1/shell_evolution_deltat_0.1yr/'
-mv Radio_curve*.png ./evolve_spectrum_epsB_1e-1/shell_evolution_deltat_0.1yr/
+python analyze_multiwavelength_spectrum.py --p_exp=2.1 --f_omega=1.00 --eps_E=0.1 --eps_B=0.1 --Te_csm=1e4 --model_dir './evolve_shock/shell_evolution_deltat_0.3yr/' --evolve_shock_dir './evolve_shock/shell_evolution_deltat_0.3yr/' --data_dir './evolve_spectrum_epsB_1e-1_p2p1/shell_evolution_deltat_0.3yr/' --dNdgamma_dir './evolve_spectrum_epsB_1e-1_p2p1/shell_evolution_deltat_0.3yr/'
+mv Radio_curve*.png ./evolve_spectrum_epsB_1e-1_p2p1/shell_evolution_deltat_0.3yr/
