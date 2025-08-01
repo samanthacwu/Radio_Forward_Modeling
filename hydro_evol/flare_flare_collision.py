5188e19eabc546cedcf54a0868d86bcc68ef5cfb
samanthacwu <samantha.c.wu@gmail.com>
2025-08-01 15:34:17 -0700

    changed how the flare_collision.py loops through inputs

--- hydro_evol/flare_flare_collision.py
+++ hydro_evol/flare_flare_collision.py
@@ -71,115 +71,115 @@ def evolve_flares(M_flares,delta_ts,v_min_c,v_max_c,p=0.5,data_dir='./evolve_spe
 	# sanity checks
 	assert p>0 # p=0 will lead to no mass ejection. p<0 is unphysical.
 	assert v_max > v_min
+	for i,delta_t in enumerate(deltat_arr):
+		for j,M_flare in enumerate(M_flare_arr):
+			
+			print(delta_t,M_flare)
 
-	for i, [delta_t,M_flare] in enumerate(zip(deltat_arr,M_flare_arr)):
-		print(delta_t,M_flare)
+			# obtain normalization of flare density profile
+			A = M_flare*(2.*p)/(4.*math.pi*v_max**3) / ((v_max/v_min)**(2.*p) - 1.)
 
-		# obtain normalization of flare density profile
-		A = M_flare*(2.*p)/(4.*math.pi*v_max**3) / ((v_max/v_min)**(2.*p) - 1.)
+			# output kinetic energy. when p=1 the integration is a bit different
+			if p == 1:
+				print('kinetic energy: %g erg' % (2.*math.pi*A*v_max**5*math.log(v_max/v_min)))
+			else:
+				print('kinetic energy: %g erg' % (2.*math.pi*A*v_max**5/(2.-2.*p) * (1.-(v_max/v_min)**(2.*p-2.))))
+			
+			# initial conditions at collision
+			t0 = v_min*delta_t / (v_max - v_min) # t defined as time from launch of later flare 
+			Msh_0 = 0.0
+			rsh_0 = v_min*v_max*delta_t / (v_max - v_min) # collision radii
+			# initial vsh set by pressure equilibrium
+			rho_flare_1_init = rho_flare(rsh_0, t0+delta_t, A, v_min, v_max)
+			rho_flare_2_init = rho_flare(rsh_0, t0, A, v_min, v_max)
+			rho12_ratio = rho_flare_2_init/rho_flare_1_init
+			vsh_0 = (v_min + v_max * math.sqrt(rho12_ratio)) / (1. + math.sqrt(rho12_ratio))
+			#vsh_0 = v_max 
 
-		# output kinetic energy. when p=1 the integration is a bit different
-		if p == 1:
-			print('kinetic energy: %g erg' % (2.*math.pi*A*v_max**5*math.log(v_max/v_min)))
-		else:
-			print('kinetic energy: %g erg' % (2.*math.pi*A*v_max**5/(2.-2.*p) * (1.-(v_max/v_min)**(2.*p-2.))))
+			# initialize
+			t = t0
+			Msh = Msh_0
+			rsh = rsh_0
+			vsh = vsh_0
+			# initialize array
+			t_arr = np.array([])
+			Msh_arr = np.array([]) 
+			rsh_arr = np.array([])
+			vsh_arr = np.array([])
+			v1_arr = np.array([])
+			v2_arr = np.array([])
+			rho1_arr = np.array([])
+			rho2_arr = np.array([])
+			rho1sq_int_arr = np.array([])
+			dMdt_arr = np.array([]) 
+			print("initial t: %s sec" % t)
+			# solve shock propagation
+			while t < 1000*t0:
+				# make sure shell doesn't expand too much at one timestep
+				dt = 1e-3*(rsh/vsh)
+				# get current rho_flare
+				rho_fl_1 = rho_flare(rsh, t+delta_t, A, v_min, v_max)
+				rho_fl_2 = rho_flare(rsh, t, A, v_min, v_max)
+				v_fl_1 = rsh/(t+delta_t)
+				v_fl_2 = rsh/t
+				# evolve
+				rsh_old = rsh # backup of rsh
+				dMdt_arr = np.append(dMdt_arr, 4.*math.pi*rsh**2*(rho_fl_2*(v_fl_2-vsh) + rho_fl_1*(vsh-v_fl_1)))
+				Msh += 4.*math.pi*rsh**2*(rho_fl_2*(v_fl_2-vsh) + rho_fl_1*(vsh-v_fl_1)) * dt
+				rsh += vsh * dt
+				vsh += (4.*math.pi*rsh_old**2/Msh) * (rho_fl_2*(v_fl_2-vsh)**2 - rho_fl_1*(vsh-v_fl_1)**2) * dt
+				t += dt
+				# append
+				t_arr = np.append(t_arr, t)
+				Msh_arr = np.append(Msh_arr, Msh)
+				rsh_arr = np.append(rsh_arr, rsh)
+				vsh_arr = np.append(vsh_arr, vsh)
+				v1_arr = np.append(v1_arr, v_fl_1)
+				v2_arr = np.append(v2_arr, v_fl_2)
+				rho1_arr = np.append(rho1_arr, rho_fl_1)
+				rho2_arr = np.append(rho2_arr, rho_fl_2)
+				# integrated rho^2 for flare 1 outside rsh (used for free-free absorption)
+				r_gtr_rsh = np.logspace(math.log10(rsh), math.log10(v_max*t+delta_t), 100)
+				rho1_gtr_rsh = [rho_flare(r, t+delta_t, A, v_min, v_max)**2 for r in r_gtr_rsh]
+				rho1sq_int_arr = np.append(rho1sq_int_arr, simpson(rho1_gtr_rsh, r_gtr_rsh))
+			# plot evolution of shell parameters
+			ax1.plot((t_arr-t0)/yr_to_sec, rsh_arr, color=color_array[i], label=r'$M_{\rm flare}=$'+f'{M_flare/Msun:1.2E} $M_\odot$, $\Delta t={delta_t/yr_to_sec:0.2f}$ yr')
+			ax2.plot((t_arr-t0)/yr_to_sec, vsh_arr/c, color=color_array[i])
+			ax3.plot((t_arr-t0)/yr_to_sec, Msh_arr/Msun, color=color_array[i])
+			ax4.plot((t_arr-t0)/yr_to_sec, dMdt_arr*yr_to_sec/Msun, color=color_array[i])
+			# save shell parameters
+			save_dir = data_dir+f'shell_evolution_Mflare_{M_flare/Msun:1.0E}_deltat_{delta_t/yr_to_sec:0.2f}yr/'
+			if not os.path.exists(save_dir):
+				os.mkdir(save_dir)
+			else:
+				print('Directory exists')
+			# np.savetxt('shell_evolution_deltat_%gyr.txt' % (delta_t/yr_to_sec), np.c_[t_arr, rsh_arr, vsh_arr, vsh_arr-v1_arr, v2_arr-vsh_arr, rho1_arr, rho2_arr,rho1sq_int_arr], header='Mfl=%gMsun, vmin=%gc, vmax=%gc, power-law-index=%g\ntime [s], rsh [cm], vsh [cm/s], Deltav_1 [cm/s], Deltav_2 [cm/s], rhofl_1 [g/cm3], rhofl_2 [g/cm3], int_rhosq_1dr [g^2/cm^5]' % (M_flare/Msun, v_min/c, v_max/c, p), fmt='%.8g')
+			np.savez(save_dir+'/shock_data.npz',
+				times=t_arr,
+				rsh_of_t=rsh_arr,
+				vsh_of_t=vsh_arr,
+				deltav1_of_t=vsh_arr-v1_arr,
+				deltav2_of_t=v2_arr-vsh_arr,
+				rho1_of_t=rho1_arr,
+				rho2_of_t=rho2_arr,
+				int_rhofl_1_sq_dr=rho1sq_int_arr
+			)
+			
+			# plot v1, v2, vshell
 
-		
-		# initial conditions at collision
-		t0 = v_min*delta_t / (v_max - v_min) # t defined as time from launch of later flare 
-		Msh_0 = 0.0
-		rsh_0 = v_min*v_max*delta_t / (v_max - v_min) # collision radii
-		# initial vsh set by pressure equilibrium
-		rho_flare_1_init = rho_flare(rsh_0, t0+delta_t, A, v_min, v_max)
-		rho_flare_2_init = rho_flare(rsh_0, t0, A, v_min, v_max)
-		rho12_ratio = rho_flare_2_init/rho_flare_1_init
-		vsh_0 = (v_min + v_max * math.sqrt(rho12_ratio)) / (1. + math.sqrt(rho12_ratio))
-		#vsh_0 = v_max 
-
-		# initialize
-		t = t0
-		Msh = Msh_0
-		rsh = rsh_0
-		vsh = vsh_0
-		# initialize array
-		t_arr = np.array([])
-		Msh_arr = np.array([]) 
-		rsh_arr = np.array([])
-		vsh_arr = np.array([])
-		v1_arr = np.array([])
-		v2_arr = np.array([])
-		rho1_arr = np.array([])
-		rho2_arr = np.array([])
-		rho1sq_int_arr = np.array([])
-		dMdt_arr = np.array([]) 
-		print("initial t: %s sec" % t)
-		# solve shock propagation
-		while t < 1000*t0:
-			# make sure shell doesn't expand too much at one timestep
-			dt = 1e-3*(rsh/vsh)
-			# get current rho_flare
-			rho_fl_1 = rho_flare(rsh, t+delta_t, A, v_min, v_max)
-			rho_fl_2 = rho_flare(rsh, t, A, v_min, v_max)
-			v_fl_1 = rsh/(t+delta_t)
-			v_fl_2 = rsh/t
-			# evolve
-			rsh_old = rsh # backup of rsh
-			dMdt_arr = np.append(dMdt_arr, 4.*math.pi*rsh**2*(rho_fl_2*(v_fl_2-vsh) + rho_fl_1*(vsh-v_fl_1)))
-			Msh += 4.*math.pi*rsh**2*(rho_fl_2*(v_fl_2-vsh) + rho_fl_1*(vsh-v_fl_1)) * dt
-			rsh += vsh * dt
-			vsh += (4.*math.pi*rsh_old**2/Msh) * (rho_fl_2*(v_fl_2-vsh)**2 - rho_fl_1*(vsh-v_fl_1)**2) * dt
-			t += dt
-			# append
-			t_arr = np.append(t_arr, t)
-			Msh_arr = np.append(Msh_arr, Msh)
-			rsh_arr = np.append(rsh_arr, rsh)
-			vsh_arr = np.append(vsh_arr, vsh)
-			v1_arr = np.append(v1_arr, v_fl_1)
-			v2_arr = np.append(v2_arr, v_fl_2)
-			rho1_arr = np.append(rho1_arr, rho_fl_1)
-			rho2_arr = np.append(rho2_arr, rho_fl_2)
-			# integrated rho^2 for flare 1 outside rsh (used for free-free absorption)
-			r_gtr_rsh = np.logspace(math.log10(rsh), math.log10(v_max*t+delta_t), 100)
-			rho1_gtr_rsh = [rho_flare(r, t+delta_t, A, v_min, v_max)**2 for r in r_gtr_rsh]
-			rho1sq_int_arr = np.append(rho1sq_int_arr, simpson(rho1_gtr_rsh, r_gtr_rsh))
-		# plot evolution of shell parameters
-		ax1.plot((t_arr-t0)/yr_to_sec, rsh_arr, color=color_array[i], label=r'$M_{\rm flare}=$'+f'{M_flare/Msun:1.2E} $M_\odot$, $\Delta t={delta_t/yr_to_sec:0.2f}$ yr')
-		ax2.plot((t_arr-t0)/yr_to_sec, vsh_arr/c, color=color_array[i])
-		ax3.plot((t_arr-t0)/yr_to_sec, Msh_arr/Msun, color=color_array[i])
-		ax4.plot((t_arr-t0)/yr_to_sec, dMdt_arr*yr_to_sec/Msun, color=color_array[i])
-		# save shell parameters
-		save_dir = data_dir+f'shell_evolution_Mflare_{M_flare/Msun:1.0E}_deltat_{delta_t/yr_to_sec:0.2f}yr/'
-		if not os.path.exists(save_dir):
-			os.mkdir(save_dir)
-		else:
-			print('Directory exists')
-		# np.savetxt('shell_evolution_deltat_%gyr.txt' % (delta_t/yr_to_sec), np.c_[t_arr, rsh_arr, vsh_arr, vsh_arr-v1_arr, v2_arr-vsh_arr, rho1_arr, rho2_arr,rho1sq_int_arr], header='Mfl=%gMsun, vmin=%gc, vmax=%gc, power-law-index=%g\ntime [s], rsh [cm], vsh [cm/s], Deltav_1 [cm/s], Deltav_2 [cm/s], rhofl_1 [g/cm3], rhofl_2 [g/cm3], int_rhosq_1dr [g^2/cm^5]' % (M_flare/Msun, v_min/c, v_max/c, p), fmt='%.8g')
-		np.savez(save_dir+'/shock_data.npz',
-			times=t_arr,
-			rsh_of_t=rsh_arr,
-			vsh_of_t=vsh_arr,
-			deltav1_of_t=vsh_arr-v1_arr,
-			deltav2_of_t=v2_arr-vsh_arr,
-			rho1_of_t=rho1_arr,
-			rho2_of_t=rho2_arr,
-			int_rhofl_1_sq_dr=rho1sq_int_arr
-		)
-		
-		# plot v1, v2, vshell
-
-		# fig0,ax0 = plt.subplots()
-		# ax0.set_xlabel(r'time from collision [yr]')
-		# ax0.set_ylabel(r'velocity [$c$]')
-		# ax0.set_xlim(1e-3, 1e2)
-		# ax0.set_xscale('log')
-		# ax0.set_title('$\Delta t=%g$ yr' % (delta_t/yr_to_sec))
-		# ax0.grid(linestyle=':')
-		# ax0.plot((t_arr-t0)/yr_to_sec, vsh_arr/c, color=color_array[i], label=r'$v_{\rm sh}$')
-		# ax0.plot((t_arr-t0)/yr_to_sec, v1_arr/c, linestyle='dashed', color=color_array[i+1], label=r'$v_1=r_{\rm sh}/(t+\Delta t)$')
-		# ax0.plot((t_arr-t0)/yr_to_sec, v2_arr/c, linestyle='dotted', color=color_array[i+2], label=r'$v_2=r_{\rm sh}/t$')
-		# ax0.legend()
-		# fig0.tight_layout()
-		# fig0.savefig('velocity_evolution_%gyr.pdf' % (delta_t/yr_to_sec))
+			# fig0,ax0 = plt.subplots()
+			# ax0.set_xlabel(r'time from collision [yr]')
+			# ax0.set_ylabel(r'velocity [$c$]')
+			# ax0.set_xlim(1e-3, 1e2)
+			# ax0.set_xscale('log')
+			# ax0.set_title('$\Delta t=%g$ yr' % (delta_t/yr_to_sec))
+			# ax0.grid(linestyle=':')
+			# ax0.plot((t_arr-t0)/yr_to_sec, vsh_arr/c, color=color_array[i], label=r'$v_{\rm sh}$')
+			# ax0.plot((t_arr-t0)/yr_to_sec, v1_arr/c, linestyle='dashed', color=color_array[i+1], label=r'$v_1=r_{\rm sh}/(t+\Delta t)$')
+			# ax0.plot((t_arr-t0)/yr_to_sec, v2_arr/c, linestyle='dotted', color=color_array[i+2], label=r'$v_2=r_{\rm sh}/t$')
+			# ax0.legend()
+			# fig0.tight_layout()
+			# fig0.savefig('velocity_evolution_%gyr.pdf' % (delta_t/yr_to_sec))
 	# finish
 	ax1.legend()
 	ax2.legend()
